d544c4becb4ed7980e3453cc154fed30edfa302d
bickhofe <peter@bickhofe.de>
2015-07-10 16:46:50 +0200

    erste version der menustruktur als klickdummy

---
+++ unity/Assets/Scripts/MyMenu.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMenu : MonoBehaviour {
+
+	public string curPage;
+	string[] pages = new string[]{"entername","checkname","welcome","selectteam","entercode","playerstat","about","scoreboard"}; 
+
+	public GameObject Logo;
+	public GameObject Subline;
+	public GameObject InputField;
+	public GameObject Teams;
+	public GameObject InfoTxt;
+	public GameObject BtnAction;
+	public GameObject ActionTxt;
+	public GameObject BtnAbout;
+	public GameObject BtnBack;
+	public GameObject WarnTxt;
+
+	public string myTeam = "unselected";  
+	
+	// Use this for initialization
+	void Start () {
+		curPage = "entername";
+		ShowPage();
+	}
+
+	public void ShowPage(){
+		if (curPage == "entername"){
+			Logo.SetActive(true);
+			InputField.SetActive(true);
+			Teams.SetActive(false);
+			
+			InfoTxt.SetActive(true);
+			InfoTxt.GetComponent<TextMesh>().text = "ENTER YOUR \nTHREE DIGIT NAME";
+			
+			BtnAction.SetActive(true);
+			ActionTxt.GetComponent<TextMesh>().text = "SUBMIT";
+		
+			BtnAbout.SetActive(true);
+			BtnBack.SetActive(false);
+			WarnTxt.SetActive(false);
+
+			//reset bei back
+			myTeam = "unselected";  
+
+		} else if (curPage == "checkname"){
+			Logo.SetActive(true);
+			InputField.SetActive(true);
+			Teams.SetActive(false);
+			
+			InfoTxt.SetActive(true);
+			InfoTxt.GetComponent<TextMesh>().text = "CHECKING...";
+			
+			BtnAction.SetActive(false);
+			BtnAbout.SetActive(false);
+			BtnBack.SetActive(false);
+			WarnTxt.SetActive(false);
+
+			StartCoroutine(Wait(3.0F));
+
+
+		} else if (curPage == "welcome"){
+			Logo.SetActive(true);
+			InputField.SetActive(true);
+			Teams.SetActive(false);
+			
+			InfoTxt.SetActive(true);
+			InfoTxt.GetComponent<TextMesh>().text = "Welcome, ABC!\nYour private secret is\nJ675J";
+			
+			BtnAction.SetActive(true);
+			ActionTxt.GetComponent<TextMesh>().text = "CONTINUE";
+
+			BtnAbout.SetActive(false);
+			BtnBack.SetActive(false);
+
+			WarnTxt.SetActive(true);
+			WarnTxt.GetComponent<TextMesh>().text = "WARNING!\nDo not loose \nyour secret code!";
+
+		} else if (curPage == "selectteam"){
+			Logo.SetActive(true);
+			InputField.SetActive(false);
+			Teams.SetActive(true);
+			
+			InfoTxt.SetActive(true);
+			InfoTxt.GetComponent<TextMesh>().text = "PLEASE,\nSELECT YOUR TEAM\n[ "+myTeam+" ] ";
+			
+			if (myTeam != "unselected") BtnAction.SetActive(true);
+			else BtnAction.SetActive(false);
+
+			ActionTxt.GetComponent<TextMesh>().text = "SELECT";
+			
+			BtnAbout.SetActive(false);
+			BtnBack.SetActive(false);
+
+			WarnTxt.SetActive(true);
+			WarnTxt.GetComponent<TextMesh>().text = "WARNING!\nYou can’t change \nyour team later!";
+		} else if (curPage == "entercode"){
+			Logo.SetActive(true);
+			InputField.SetActive(true);
+			Teams.SetActive(false);
+			
+			InfoTxt.SetActive(true);
+			InfoTxt.GetComponent<TextMesh>().text = "ABC is already in \nuse. Please enter \nyour secret code.";
+			
+			BtnAction.SetActive(true);
+			ActionTxt.GetComponent<TextMesh>().text = "LOGIN";
+			
+			BtnAbout.SetActive(false);
+			BtnBack.SetActive(true);
+			
+			WarnTxt.SetActive(false);
+		}
+	}
+
+	IEnumerator Wait(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		curPage = "welcome";
+		ShowPage();
+	}
+}
---
+++ unity/Assets/Scripts/TouchObjects.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchObjects : MonoBehaviour {
+
+	MyMenu mainScript;
+	string stringToEdit = "Debug";
+
+	void Start(){
+		mainScript = GetComponent<MyMenu>();
+	}
+
+	void Update () {
+
+		//mouse
+		if (Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitClick;
+
+			if (Physics.Raycast(ray, out hitClick)){
+				stringToEdit = "Touched: "+hitClick.collider.name;
+				Debug.Log ("Touched: "+hitClick.collider.name);
+				Click (hitClick.collider.name);
+			}
+		}
+			
+			// touch
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint((Input.GetTouch (0).position)), Vector2.zero);
+
+			if (hit.collider != null)
+			{
+				stringToEdit = "Touched: "+hit.collider.name;
+				Debug.Log ("Touched: "+hit.collider.name);
+				Click (hit.collider.name);
+			}
+		}
+	}
+
+	void Click(string Target){
+		if (mainScript.curPage == "entername"){
+			if (Target == "ButtonAction") mainScript.curPage = "checkname";
+			if (Target == "ButtonAbout") mainScript.curPage = "about";
+		}
+
+		else if (mainScript.curPage == "welcome"){
+			if (Target == "ButtonAction") mainScript.curPage = "selectteam";
+		}
+
+		else if (mainScript.curPage == "selectteam"){
+			if (Target == "space") mainScript.myTeam = "Invaders";
+			if (Target == "pac") mainScript.myTeam = "Pac Men";
+			if (Target == "galaga") mainScript.myTeam = "Galagas";
+
+			if (Target == "ButtonAction") mainScript.curPage = "entercode";
+		}
+
+		else if (mainScript.curPage == "entercode"){
+			if (Target == "ButtonAction") mainScript.curPage = "entercode";
+			if (Target == "ButtonBack") mainScript.curPage = "entername";
+		}
+
+		mainScript.ShowPage();
+	}
+
+	void OnGUI() {
+		stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+	}
+}
