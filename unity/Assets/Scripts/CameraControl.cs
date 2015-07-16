b945d03b6b90db0441bc6fb7b8b328df21fa1fae
bickhofe <peter@bickhofe.de>
2015-07-16 13:51:50 +0200

    out of gaming grid und follow cam

---
+++ unity/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform Target;
+	bool inRange = false;
+
+	float camX;
+	float camZ;
+	
+	public Camera myCam;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inRange){
+			camX = Target.position.x;
+			if (Target.position.x < -2.5f) camX = -2.5f;
+			if (Target.position.x > 2.5f) camX = 2.5f;
+
+			camZ = Target.position.z;
+			if (Target.position.z < -4.5f) camZ = -4.5f;
+			if (Target.position.z > 4.5f) camZ = 4.5f;
+
+			transform.position = new Vector3(camX,8.5f,camZ);
+			myCam.orthographicSize = 5;
+		} else {
+			myCam.orthographicSize = 9.5f;
+		}
+
+
+		//out of evoke
+		if (Target.position.x < -5 || Target.position.x > 5 || Target.position.z < -9 || Target.position.z > 9) inRange = false;
+		else inRange = true; 
+	}
+}
--- unity/Assets/Scripts/NavToPosition.cs
+++ unity/Assets/Scripts/NavToPosition.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NavToPosition : MonoBehaviour {
@@ -8,16 +9,18 @@ public class NavToPosition : MonoBehaviour {
 
 	public float lat = 50.944303f;
 	public float lon =  6.937723f;
-	public float multiX = 3750;
-	public float multiY = 7930;
+	public float multiX = 4500;
+	public float multiY = 7000;
 
 	float posX;
 	float posZ;
 
-	int count = 0;
-
 	public GameObject player;
 	Vector3 playerRotation;
+
+	public int idleSpeed;
+	bool inRange = false;
+	public GameObject OutText;
 
 	// Use this for initialization
 	void Start () {
@@ -27,29 +30,42 @@ public class NavToPosition : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (myGPS.gpsReady){
-			count++;
-			posX = (Input.location.lastData.longitude - lon)*4500;
-			posZ = (Input.location.lastData.latitude - lat)*7000;
-
-			player.transform.position = new Vector3(posX,0,posZ);
-
+			posX = (Input.location.lastData.longitude - lon)*multiX;
+			posZ = (Input.location.lastData.latitude - lat)*multiY;
 			playerRotation = new Vector3(0,Input.compass.trueHeading,0);
-			player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler(playerRotation), Time.deltaTime * 1.0f); 
-			//player.transform.rotation = Quaternion.Euler(0, Input.compass.trueHeading, 0);
-	
-
 		} else {
-			count++;
-			posX = (6.937723f - lon)*4500;
-			posZ = (50.944303f - lat)*7000;
+			posX = (6.937723f - lon)*multiX;
+			posZ = (50.944303f - lat)*multiY;
+			playerRotation = new Vector3(0,0,0);
+
+			//posX = -6; 
+			//posZ = -11;
 
 			// mitte: 50.944303 - 6.937723
 			// oben rechts: 50.945627 - 6.938738
 			// unten rechts: 50.943015 - 6.938892
-			
-			player.transform.position = new Vector3(posX,0,posZ);
 		}
 
-		DebugTextfield.GetComponent<TextMesh>().text = count+" "+Input.compass.trueHeading+"\n"+posX+" "+posZ;
+		if (inRange){
+			player.transform.localScale = new Vector3(.5f,.5f,.5f);
+			player.transform.position = new Vector3(posX,0,posZ);
+			player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.Euler(playerRotation), Time.deltaTime * 1.0f);
+			OutText.SetActive(false);
+		} else {
+			outOfEvoke();
+		}
+
+		//out of evoke
+		if (posX < -5 || posX > 5 || posZ < -9 || posZ > 9) inRange = false;
+		else inRange = true; 
+
+		DebugTextfield.GetComponent<Text>().text = inRange+" "+Input.compass.trueHeading+"\n"+posX+" "+posZ;
+	}
+
+	void outOfEvoke(){
+		OutText.SetActive(true);
+		player.transform.localScale = new Vector3(4,4,4);
+		player.transform.position = new Vector3(0,5,0);
+		player.transform.Rotate(new Vector3(Time.deltaTime * idleSpeed,Time.deltaTime * idleSpeed,0));
 	}
 }
