a9806b6fa6a6ae962a4db16ea4b26d0910da3afb
bickhofe <peter@bickhofe.de>
2015-07-23 14:58:10 +0200

    generate random icons

--- unity/Assets/Scripts/GetGameData.cs
+++ unity/Assets/Scripts/GetGameData.cs
@@ -3,34 +3,53 @@ using System.Collections;
 
 public class GetGameData : MonoBehaviour {
 
+
 	public GameObject[] allGoodies;
-	public int maxGoodies = 5;
+	public string[] goodieStat;
 	int goodieID;
 
 
 	// Use this for initialization
 	void Start () {
-		chooseGoodies();
+		updateGoodies();
 	}
 
-	void chooseGoodies(){
-		for (int i=0; i<maxGoodies; i++){
-			int rnd = Random.Range(0,100);
-			
-			if (rnd == 100) goodieID = 9;
-			else if (rnd >= 98 && rnd < 100) goodieID = 8;
-			else if (rnd >= 95 && rnd < 98)  goodieID = 7;
-			else if (rnd >= 90 && rnd < 95)  goodieID = 6;
-			else if (rnd >= 80 && rnd < 90)  goodieID = 5;
-			else if (rnd >= 70 && rnd < 80)  goodieID = 4;
-			else if (rnd >= 50 && rnd < 60)  goodieID = 3;
-			else if (rnd >= 40 && rnd < 50)  goodieID = 2;
-			else if (rnd >= 20 && rnd < 40)  goodieID = 1;
-			else if (rnd >= 0 && rnd < 20)  goodieID = 0;
+	void updateGoodies(){
+		for (int i=0; i<goodieStat.Length; i++){
+			if (goodieStat[i] == "available"){
+				GameObject newGoodie = Instantiate(allGoodies[selectGoodieType()], transform.position, Quaternion.identity) as GameObject;
+				newGoodie.transform.parent = transform;
+				newGoodie.transform.localEulerAngles = new Vector3(90,0,0);
 
-			print(goodieID);
+				//spawnzone auswählen
+				if (Random.value < 0.5f) {
+					// zone 0
+					newGoodie.transform.position = new Vector3(Random.Range(-2.5f,0.5f),.1f,Random.Range(0,5.5f));
+				} else {
+					// zone 1
+					newGoodie.transform.position = new Vector3(Random.Range(2.5f,4.5f),.1f,Random.Range(6.5f,-7.5f));
+				}
+
+				//set goodie params
+				goodieStat[i] = "active";
+				print(Random.value);
+			}
 		}
+	}
 
+	int selectGoodieType(){
+		int rnd = Random.Range(0,100);
 
+		if (rnd == 100) goodieID = 9;
+		else if (rnd >= 98 && rnd < 100) goodieID = 8;
+		else if (rnd >= 95 && rnd < 98)  goodieID = 7;
+		else if (rnd >= 90 && rnd < 95)  goodieID = 6;
+		else if (rnd >= 80 && rnd < 90)  goodieID = 5;
+		else if (rnd >= 70 && rnd < 80)  goodieID = 4;
+		else if (rnd >= 50 && rnd < 60)  goodieID = 3;
+		else if (rnd >= 40 && rnd < 50)  goodieID = 2;
+		else if (rnd >= 20 && rnd < 40)  goodieID = 1;
+		else if (rnd >= 0 && rnd < 20)  goodieID = 0;
+		return goodieID;
 	}
 }
