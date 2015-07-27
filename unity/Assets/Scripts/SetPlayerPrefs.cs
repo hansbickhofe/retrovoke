624806f13bb65d14248e62e8e95ace785aa9e8d3
Hans Bickhofe <hbickhofe@gmail.com>
2015-07-27 18:51:48 +0200

    DEMO Players in Comments notiert

--- unity/Assets/Scripts/SetPlayerPrefs.cs
+++ unity/Assets/Scripts/SetPlayerPrefs.cs
@@ -6,6 +6,11 @@ public class SetPlayerPrefs : MonoBehaviour {
 	public string playercode ;
 	public int playerteam ;
 	// Use this for initialization
+	// HOB == MXAJ147321ZW 
+	// HHH == HXSJ947537HT
+	// PPP == WCXU877693KA
+	// PET == MXWH558618KY
+	// PED == NNYG696505WZ	
 	void Start () {
 		PlayerPrefs.SetString("playername","HOB");
 		PlayerPrefs.SetString("playercode","MXAJ147321ZW");
