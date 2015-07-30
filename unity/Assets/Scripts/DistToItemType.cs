cb9f726b01dabd9b72f597d0cc7a66bd98e72585
bickhofe <peter@bickhofe.de>
2015-07-30 23:08:20 +0200

    random item

---
+++ unity/Assets/Scripts/DistToItemType.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistToItemType : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distToItemType();
+	}
+
+	void distToItemType(){
+
+		float rndValueX = Random.Range (-10f, 10f);
+		float rndValueY = Random.Range (-10f, 10f);
+
+		int itemType = 0;
+		//inner circle
+		if (rndValueX > -5 && rndValueX < 3 && rndValueY > -3 && rndValueY < 3) {
+			itemType = Random.Range (0,4);
+		} else if (rndValueX > -7.5f && rndValueX < 7.5f && rndValueY > -7.5f && rndValueY < 7.5f) {
+			itemType = Random.Range (5,7);
+		} else if (rndValueX >= -10f && rndValueX <= 10f && rndValueY >= -10 && rndValueY <= 10) {
+			itemType = Random.Range (8,10);
+		}
+
+		Debug.Log (itemType);
+	}
+}
